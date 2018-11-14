019d6b4dcb621acac659412e60650c08a741ed77
kskoushik26 <44638962+kskoushik26@users.noreply.github.com>
2018-11-14 18:08:18 +0530

    javaprogram

---
+++ numbersproject/src/com/proj/num/SecondjavaProg.java
@@ -0,0 +1,18 @@
+package com.proj.num;
+
+public class SecondjavaProg 
+{
+	public static void main(String[] args) {
+		for (int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+			System.out.println("the number is even"+i);
+			}
+			else {
+				System.out.println("the number is odd "+i);
+			}
+		}
+	}
+
+}
